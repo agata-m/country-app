99bedcb1794f4470a2a6894aabe6f6e141d7b04e
agata-m <49907570+agata-m@users.noreply.github.com>
2019-10-24 18:48:58 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import {Router, hashHistory} from 'react-router';
 import routes from './routes';
 import { getCountries } from './actions/actions-countries';
 
+import style from './country.css';
+
 render (
-    <Provider store={store}>
+    <Provider store={store} style={style}>
         <div>
             <Router history={hashHistory} routes={routes} />
             <DevTools />
